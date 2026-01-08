4007341d48d76b1be55a9d626822800a1c4acd82
scottyler89 <scottyler89@gmail.com>
2026-01-08 23:23:43 +0000

    Packaging: improve setup.py back-compat

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-# Read the content of your requirements.txt file
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required_packages = f.read().splitlines()
 
-setup(
-    name='guide_pmd',
-    version='0.1.4',
-    #packages=find_packages(exclude=('tests*', 'testing*')),
-    license='MIT',
-    description='A package for analyzing CRISPR screens (or similar data), using PMD standardized residuals with linear modeling downstream',
-    long_description=open('README.md').read(),
-    install_requires=required_packages,
-    url='https://github.com/scottyler89/guide_pmd_std_res',
-    author='Scott Tyler',
-    author_email='scottyler89@gmail.com'
-)
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
+setup(
+    name="guide_pmd",
+    version="0.1.4",
+    packages=find_packages(),
+    license="MIT",
+    description="A package for analyzing CRISPR screens (or similar data), using PMD standardized residuals with linear modeling downstream",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=required_packages,
+    python_requires=">=3.10",
+    url="https://github.com/scottyler89/guide_pmd_std_res",
+    author="Scott Tyler",
+    author_email="scottyler89@gmail.com",
+)
 
