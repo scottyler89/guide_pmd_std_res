d3f6e8c4e8b28bf22dbfb9f434ed8d4bf6ea6785
scottyler89 <scottyler89@gmail.com>
2026-01-13 19:26:13 +0000

    Assert default gene-level outputs include robustness artifacts

--- tests/test_baseline_golden.py
+++ tests/test_baseline_golden.py
@@ -51,5 +51,11 @@ def test_baseline_outputs_match_golden(tmp_path, monkeypatch):
     gene_level_dir = out_dir / "gene_level"
     assert gene_level_dir.is_dir()
     assert (gene_level_dir / "PMD_std_res_gene_meta.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_lmm_selection.tsv").is_file()
     assert (gene_level_dir / "PMD_std_res_gene_lmm.tsv").is_file()
     assert (gene_level_dir / "PMD_std_res_gene_qc.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_flagged.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_mixture.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_mixture_guides.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_tmeta.tsv").is_file()
+    assert (gene_level_dir / "PMD_std_res_gene_tmeta_guides.tsv").is_file()
